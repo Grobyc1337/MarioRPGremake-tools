310fb8ff6c57f5198caa383365ae0b6793ffc849
Guillaume <Guillaumedefourz@gmail.com>
2023-12-26 22:59:14 -0500

    Updated Issues with add_text_action_name_tool.py

--- menu_text_eng_us_tool/add_text_action_name_tool.py
+++ menu_text_eng_us_tool/add_text_action_name_tool.py
@@ -12,16 +12,23 @@ from json.decoder import JSONDecodeError
 #               FUNCTIONS
 ##########################################
 
+
 def create_new_cwi_action(a_object_id, m_skill_id, string_id, string_name):
     return ["ClassWithId", {"ObjectId": a_object_id, "MetadataId": 33, "Values": [["m_id : Primitive", m_skill_id],
         ["m_text : String", ["BinaryObjectString", {"ObjectId": string_id, "Value": string_name}]]]}]
 
+
 def create_new_idref(new_idref):
     return ["MemberReference", {"IdRef": new_idref}]
+
+
+def create_new_objectnull():
+    return ['ObjectNull', None]
 
 ##########################################
 #               CLASSES
 ##########################################
+
 
 class ObjectType(Enum):
     ACTION_NAME = 1
@@ -30,30 +37,22 @@ class ObjectType(Enum):
 #               BEGIN
 ##########################################
 
+
 with open('menu_text_eng_us.json', 'r') as input_file:
     data = json.load(input_file)
-
-#input_file = sys.argv[1]
-
-#file_reader = open(input_file, 'r')
-#data = json.load(file_reader)
-
-
-# Ask user how many Action's Name they want to add
-# amount_to_add = input("How many new Action's Name would you like to add? : ")
-# display_amount_changed = amount_to_add  # Keep the str() to display it later
 
 
 # Adding a bunch of Var
 new_list = False      # Asking if the list is genuine
 the_list_2 = []       # Create a second list that we will use
-new_list_test = []
+new_list_test = []    # Calculate Length with this
 check_highest_id = []
 check_highest_ba_id = []
 max_new_id = 0
 max_new_ba_id = 0
 last_o_id = 0
 d_object_id = 0
+id_missing = True
 # Maxtoad's Vars
 object_index = -1
 objects = {}
@@ -126,8 +125,8 @@ for action in actions:
 
 # ~~~ END MAXTOAD's MASTER CODE ~~~
 
-# ~~~ MON CODE START ~~~
-# Find length of Table action_name  (answer is 750)
+# ~~~ GROBYC's CODE START ~~~
+# Find length of Table action_name  (answer is 750 if new list)
 
 for item in data.values():
     item_class = item[0]
@@ -136,13 +135,14 @@ for item in data.values():
         new_list_test.append(d_object_id)
 for classes in new_list_test:
     if classes > last_o_id:
-        last_o_id = classes
+        last_o_id = classes  # Last Object ID
 
-last_skill_id = objects_by_type['action_name'][0][1]['Lengths'][0]
-# Find last string_id in action_name list
-last_action_string_id = actions[-1][1]['Values'][1][1][1]['ObjectId']
+the_missing_id = create_new_objectnull()  # Must add a missing IdRef for spell #750 else offset is bad
+last_skill_id = objects_by_type['action_name'][0][1]['Lengths'][0]     # Check list length
+last_action_string_id = actions[-1][1]['Values'][1][1][1]['ObjectId']  # Find last string_id in action_name list
 
-# Ask user how many Action's Name they want to add
+
+# Ask user how many Action's Name(s) they want to add
 amount_to_add = input("How many new Action's Name would you like to add? : ")
 display_amount_changed = amount_to_add  # Keep the str() to display it later
 try:
@@ -153,29 +153,27 @@ except ValueError:
     sys.exit()
 
 
-
 # those vars are only used if user is adding more than 1 skill
 i = 1  # i  is for the ID to add when looping
 j = 1  # j  is for the amount in first loop
 k = 0  # k  is for the amount in second loop
 new_index_idref = 0
 # Use this to see if list has been edited or not! (Original length or not)
-if last_skill_id == 750:   # Means list has Original length -> Use 50000
+if last_skill_id == 750:   # Means list has Original length -> Use 50000 for new offset
     new_list = True
     last_object_id = 50000    # This is a custom new ObjectId for the Spell => 50000 is unused
     last_string_id = 60000    # This is a custom new ObjectId for Spell's Strings => 60000 is unused
 else:
-    last_object_id = last_o_id + 1  # last_skill_id          # last action skill ID in the list
-    last_string_id = last_action_string_id  # last action string in the list
-# Use a different value if the list already has been edited !!!
+    id_missing = False
+    last_object_id = last_o_id + 1  # last action skill ID in the list + 1 for new Id
+    last_string_id = last_action_string_id + 1  # last action string in the list + 1 for new Id
 
-blank_new_name = 'Blank Name'  # Put blank name when creating more than 1 spell
+blank_new_name = 'Blank Name'  # Put blank name when creating more than 1 spell *Might change in the future*
 
 # ~~~~~~~~  If input is > 1 -> Generate "x" Blank Name Spells  ~~~~~~~~
 
 amount_to_add = int(amount_to_add)
 if amount_to_add > j:  # j = 1  here
-
     while j <= amount_to_add:
           # Unedited list starts at 751 (750 + 1)
         if new_list:  # offset the new objectid & stringid if needed
@@ -193,9 +191,6 @@ if amount_to_add > j:  # j = 1  here
         new_cwi_action = create_new_cwi_action(new_name_id, new_skill_id, new_string_id, blank_new_name)
         actions.append(new_cwi_action)  # This works well
 
-        #  index 3162 should be a variable
-        #  last_object_id
-
         for item2 in the_list_2:
             item_class_2 = item2[0]
             item_ba_class = item2[0]
@@ -208,9 +203,6 @@ if amount_to_add > j:  # j = 1  here
                 check_highest_ba_id.append(object_ba_id)
                 if object_ba_id == 16:
                     new_index_idref = the_list_2.index(item2)
-            #    for ba_ids in check_highest_ba_id:
-            #        if ba_ids > max_new_ba_id:
-            #            max_new_ba_id = ba_ids  # Return highest BinaryArray ID
 
         for ids in check_highest_id:
             if ids > max_new_id:
@@ -223,8 +215,14 @@ if amount_to_add > j:  # j = 1  here
                 if max_new_id < 50000:  # Means list is fresh
                     new_index_add = 3163
                 else:
-                    new_index_add = 3163 + (last_skill_id - 750) * 2
+                    new_index_add = 3163 + (last_skill_id - 750) * 2 + 1
 
+        if id_missing:
+            missing_index_idref = new_index_idref
+            the_list_2.insert(missing_index_idref, the_missing_id)
+            id_missing = False
+            new_index_idref += 1  # Offset by 1
+            new_index_add += 1
         while k < amount_to_add:
             new_skill_id = last_skill_id + (k + 1)
             new_name_id = last_object_id + k
@@ -236,29 +234,18 @@ if amount_to_add > j:  # j = 1  here
             the_list_2.insert(new_index_add, new_cwi_action2)
             new_index_add += 2    # Adjust Order
             new_index_idref += 1  # Adjust Order
-            k += 1 # Counter for amount to add
-
+            k += 1  # Counter for amount to add
 
     for item2 in the_list_2:
         item_ba_class = item2[0]
         if item_ba_class == 'BinaryArray':
             object_ba_id = item2[1]['ObjectId']
             check_highest_ba_id.append(object_ba_id)
-            # amount_to_add = int(amount_to_add)
-            # print(item_ba[1]['Lengths'])
             if object_ba_id == 14:
                 item2[1]['Lengths'][0] = item2[1]['Lengths'][0] + amount_to_add
 
             if object_ba_id == 16:
                 new_index_idref = the_list_2.index(item2)
-            #for ba_ids in check_highest_ba_id:
-            #    if ba_ids > max_new_ba_id:
-            #        max_new_ba_id = ba_ids  # Return highest BinaryArray ID
-
-
-
-
-    #find_actioname = the_list_2[3161]
 
     print("""
     You added """ + display_amount_changed + """ new skill's names!
@@ -266,10 +253,7 @@ if amount_to_add > j:  # j = 1  here
     """)
 
 
-
 # ~~~~~~~~  If input is 1   -> prompt "enter custom name" ~~~~~~~~
-
-
 
 else:
     new_name_to_add = input('Input your new Action Name : ')
@@ -278,8 +262,8 @@ else:
         new_name_id = last_object_id
         new_string_id = last_string_id
     else:
-        new_name_id = last_object_id + i
-        new_string_id = last_string_id + i
+        new_name_id = last_object_id #+ i
+        new_string_id = last_string_id
 
     for item2 in the_list_2:
         item_class_2 = item2[0]
@@ -297,16 +281,11 @@ else:
         if item_class_3 == 'ClassWithId':
             object_id2 = item3[1]['ObjectId']
             check_highest_id.append(object_id2)
-
             if max_new_id < 50000:  # Means list is fresh
                 new_index_add = 3162
             else:
-                #new_index_add = maxvalue
-                #new_index_add += 1
                 if item3[1]['ObjectId'] == max_new_id:
-                    new_index_add = the_list_2.index(item3[1]['ObjectId'])
-
-
+                    new_index_add = the_list_2.index(item3) + 1
 
     new_cwi_action = create_new_cwi_action(new_name_id, new_skill_id, new_string_id, new_name_to_add)
     # This append the new action(s) name(s) at the good place in actions!
@@ -314,17 +293,7 @@ else:
     # This insert the new action(s) name(s) at the good place in the_list_2!
     the_list_2.insert(new_index_add, new_cwi_action)
 
-
-    # Must build an ID Ref  at the corresponding place ->   ex :  "ObjectId": 191  is "IdRef": 191
-    # ->  Find highest IdRef in BinaryArray(ObjectId 14)
-
-    # if highest_ba_idref == 191 # Fresh List ->
-    #    #create memberref with IdRef
-    # else:
-    # Find highest_ba_idref
-    # eg:
-    # highest_ba_objectid = 50005 -> do + 1
-
+    # This is to insert the IdRef at the good index when building the new list
     for item_ba in the_list_2:
         item_ba_class = item_ba[0]
         if item_ba_class == 'BinaryArray':
@@ -333,29 +302,31 @@ else:
             #amount_to_add = int(amount_to_add)
             #print(item_ba[1]['Lengths'])
             if object_ba_id == 14:
-                item_ba[1]['Lengths'][0] = [item_ba[1]['Lengths'][0] + amount_to_add]
+                item_ba[1]['Lengths'][0] = item_ba[1]['Lengths'][0] + amount_to_add
 
             if object_ba_id == 16:
                 new_index_idref = the_list_2.index(item_ba)
 
-    #for ba_ids in check_highest_ba_id:
-    #    if ba_ids > max_new_ba_id:
-    #        max_new_ba_id = ba_ids  # Return highest BinaryArray ID
+    if id_missing:
+        missing_index_idref = new_index_idref
+        the_list_2.insert(missing_index_idref, the_missing_id)
+        id_missing = False
+        new_index_idref += 1  # Offset by 1
 
     new_idref = create_new_idref(new_name_id)
     the_list_2.insert(new_index_idref, new_idref)
 
     print("""
     You added """ + new_name_to_add + """ Skill Name!
-    Bubye for now!
-    """)
+    """)  # looks better on the output
 
 
-    print("Press any key to finish the program : ")
-    print("Doesnt work yet,lol execution...")
-    #input()
-
-
+#stay_active = input('Do you want to add another spell(s)? Yes / No : ')
+# stay_active.upper() != "Y" and "YES":
+#    active = False  # Stop Program
+#else:
+#    active = True  # Reset-Continue Program
+#    re_add = True
 
 output_file = 'menu_text_eng_us_added.json'
 #output_file = os.path.relpath(input_file.replace('_simplified.json', '_added.json'), os.getcwd())
@@ -363,24 +334,5 @@ file_writer = open(output_file, 'w')
 file_writer.write(json.dumps(the_list_2, indent=2))
 
 
-"""
-    for item2 in the_list_2:
-        item_class_2 = item2[0]
-        item_value_2 = item2[1]
-        if item_class_2 == 'ClassWithId':
-            object_id2 = item2[1]['ObjectId']
-            check_highest_id.append(object_id2)
-
-    maxvalue = check_highest_id[0]
-    for number in check_highest_id:
-        if number > maxvalue:
-            maxvalue = number
-
-    if maxvalue < 50000:  # Means list is fresh
-        new_index_add = 3162
-    else:
-            #new_index_add = maxvalue
-            #new_index_add += 1
-        if item2[1]['ObjectId'] == max_new_id:
-            new_index_add = the_list_2.index(item2[1]['ObjectId'])
-"""
+print("Congratulation! Press any key to finish the program : ")
+input()
