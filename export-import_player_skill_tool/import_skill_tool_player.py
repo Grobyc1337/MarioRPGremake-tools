56a7feb316691964870dbc606b5344856dc61615
Guillaume <Guillaumedefourz@gmail.com>
2024-01-19 14:26:28 -0500

    Update #2 new extract skill tool

---
+++ export-import_player_skill_tool/import_skill_tool_player.py
@@ -0,0 +1,60 @@
+
+# Made by Maxtoad & Grobyc
+
+import json
+import sys
+from typing import Dict, List
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        raise Exception("Pass in an argument for input file")
+
+    input_file = sys.argv[1]
+    output_file = input_file.replace('.json','_imported.json')
+
+
+    skill_to_import = input("Which Skill would you like to import? : ")
+    display_skill_imported = skill_to_import  # Keep the str() to display it later
+    try:
+      val = int(skill_to_import)
+    except ValueError:
+      print("That's not an int!")
+      print("Stop it....get some help")
+      sys.exit()
+
+    skill_to_import = int(skill_to_import)
+
+    with open('Skill_' + display_skill_imported + '.json', 'r') as input_file2:
+        data2 = json.load(input_file2)
+
+
+
+    #print(data2)
+
+    objects = []
+    list_data2 = []
+    i = 0
+    with open(input_file, 'r') as actions_f:
+      actions = json.load(actions_f)
+
+      for items in data2:
+          item_type = items
+          list_data2.append(items)
+
+      for value in actions.values():
+          elem_type = value[0]
+          if elem_type != 'MemberReference':
+            if elem_type == 'ClassWithId':
+              if value[1]['Values'][1][1] == skill_to_import:
+                  objects.append(list_data2[i])
+                  i += 1
+              else:
+                  objects.append(value)
+            else:
+                objects.append(value)
+    with open(output_file, 'w') as output:
+      output.write(json.dumps(objects, indent=2))
+
+    print("""
+    You Succesfuly Imported Skill ID """ + display_skill_imported)
